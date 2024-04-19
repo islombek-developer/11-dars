17718499dcacf5e967ef4e54a777b022282ccdd9
islombek-developer <164884102+islombek-developer@users.noreply.github.com>
2024-04-19 21:24:44 +0300

    s

--- salom.py
+++ salom.py
@@ -1,32 +1,62 @@
-class CustomRange:
-    def __init__(self, start, end, step=None):
-        self.current = start
-        self.end = end
-        self.step = step
+# class CustomRange:
+#     def __init__(self, start, end, step=None):
+#         self.current = start
+#         self.end = end
+#         self.step = step
 
-    def __iter__(self):
-        return self
+#     def __iter__(self):
+#         return self
+#     def __next__(self):
+#         if self.current <= self.end:
+#             result = self.current
 
-    def __next__(self):
-        if self.current <= self.end:
-            result = self.current
+#             if self.step is None:
+#                 self.current+=1
+#             else:
+#                 self.current+=self.step
+#             return result
+#         else:
+#             raise StopIteration
 
-            if self.step is None:
-                self.current+=1
-            else:
-                self.current+=self.step
-            return result
+# for num in CustomRange(4,9):
+#     print(num)
+
+
+
+
+
+# def quicksort(x):
+#     if len(x) <= 1:
+#         return x
+#     else:
+#         a = x[0]
+#         lesser = [i for i in x[1:] if i <= a]
+#         greater = [i for i in x[1:] if i > a]
+#         return quicksort(greater) + [a] + quicksort(lesser)
+
+def binary_search(x, target):
+    c=0
+    if len(x) <= 1:
+        return x
+    else:
+        a = x[0]
+        lesser = [i for i in x[1:] if i <= a]
+        greater = [i for i in x[1:] if i > a]
+        c=binary_search(greater, target) + [a] + binary_search(lesser, target)
+    start = 0
+    end = len(c) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        mid_value = c[mid]
+        if mid_value == target:
+            return mid
+        elif mid_value > target:
+            end = mid - 1
         else:
-            raise StopIteration
+            start = mid + 1
 
-
-for num in CustomRange(4,9):
-    print(num)
-
-
-
-
-
+d = [3, 6, 8, 10, 1, 2, 1]
+print(binary_search(d, 2))
 
 
 
